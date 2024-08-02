c0c8dc3b948005cb7d98df5eb2d660f676809fbb
Shun <s.tsuchiya.business@gmail.com>
2024-08-02 16:59:59 +0900

    change names

--- configs/nvim/lua/config/plugins/tokyo_night.lua
+++ configs/nvim/lua/config/plugins/tokyo_night.lua
@@ -1,6 +1,6 @@
 require("tokyonight").setup({
   -- your configuration comes here
-  -- or leave it empty to use the default settings
+  -- or leave it empty to use the default configs
   style = "moon", -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
   light_style = "day", -- The theme is used when the background is set to light
   transparent = true, -- Enable this to disable setting the background color
@@ -37,5 +37,5 @@ require("tokyonight").setup({
   on_highlights = function(highlights, colors) end,
 })
 
--- Attach above settings
+-- Attach above configs
 vim.cmd[[colorscheme tokyonight]]
