fb43fc28d7bd46ff3d98616bd815dd762312370a
shunsock <s.tsuchiya.business@gmail.com>
2024-08-15 13:34:50 +0900

    change wezterm_setting

--- configs/wezterm/wezterm.lua
+++ configs/wezterm/wezterm.lua
@@ -3,7 +3,7 @@ local wezterm = require 'wezterm'
 local config = {}
 
 config.font = wezterm.font(
-  "Moralerspace Argon HWNF",
+  "DroidSansMNerdFontMono-Regular"
   {
     weight="Regular",
     stretch="Normal",
@@ -18,7 +18,7 @@ config.color_scheme = 'Tokyo Night'
 
 config.window_background_opacity = 0.9
 config.window_decorations = 'RESIZE'
-config.window_padding = { left = 15, right = 15, top = 0, bottom = 0 }
+config.window_padding = { left = 32, right = 32, top = 0, bottom = 64 }
 
 config.use_fancy_tab_bar = false
 config.tab_and_split_indices_are_zero_based = true
