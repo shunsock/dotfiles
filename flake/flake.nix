4ebf9be9ebab23f6e2ce91c90abeeb09132794c3
Shun <s.tsuchiya.business@gmail.com>
2024-10-20 18:12:55 +0900

    fix flake

--- flake/flake.nix
+++ flake/flake.nix
@@ -8,11 +8,15 @@
   outputs = { self, nixpkgs }: {
 
     # Define package for arm64-darwin (Apple Silicon Mac)
-    packages.arm64-darwin.hello = nixpkgs.legacyPackages.aarch64-darwin.hello;
+    packages.aarch64-darwin.hello = nixpkgs.legacyPackages.aarch64-darwin.hello;
 
-    # Set the default package for arm64-darwin
-    defaultPackage.arm64-darwin = self.packages.arm64-darwin.hello;
+    # Set the default package for aarch64-darwin
+    defaultPackage.aarch64-darwin = self.packages.aarch64-darwin.hello;
 
+    # Optionally, define an app (nix run looks for this)
+    apps.aarch64-darwin.hello = {
+      type = "app";
+      program = "${self.packages.aarch64-darwin.hello}/bin/hello";
+    };
   };
 }
-
