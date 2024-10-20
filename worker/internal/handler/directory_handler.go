23b457f3e876be5760e790626d0c81e1a1324977
Shun <s.tsuchiya.business@gmail.com>
2024-10-20 12:56:57 +0900

    pathの解決を正しいものにする

--- worker/internal/handler/directory_handler.go
+++ worker/internal/handler/directory_handler.go
@@ -1,20 +1,24 @@
 package handler
 
 import (
-  "worker/internal/core/path"
+  "os/exec"
+  "worker/internal/path"
 )
 
 // ディレクトリ操作
 type DirectoryHandler struct{}
 
 // CopyRecursive: ディレクトリを再帰的にコピー (DirectoryPathを使用)
-func (dh *DirectoryHandler) CopyRecursive(src *DirectoryPath, dst *DirectoryPath) error {
+func (dh *DirectoryHandler) CopyRecursive(
+  src *path.DirectoryPath,
+  dst *path.DirectoryPath,
+) error {
 	cmd := exec.Command("cp", "-r", src.Path, dst.Path)
 	return cmd.Run()
 }
 
 // RemoveRecursive: ディレクトリを再帰的に削除 (DirectoryPathを使用)
-func (dh *DirectoryHandler) RemoveRecursive(dir *DirectoryPath) error {
+func (dh *DirectoryHandler) RemoveRecursive(dir *path.DirectoryPath) error {
 	cmd := exec.Command("rm", "-r", dir.Path)
 	return cmd.Run()
 }
--- worker/internal/handler/file_handler.go
+++ worker/internal/handler/file_handler.go
@@ -1,20 +1,26 @@
 package handler
 
 import (
-  "worker/internal/core/path"
+  "os/exec";
+  "worker/internal/path"
 )
 
 // ファイル操作
 type FileHandler struct{}
 
 // Copy: ファイルをコピーする
-func (fh *FileHandler) Copy(src *FilePath, dst *FilePath) error {
+func (fh *FileHandler) Copy(
+  src *path.FilePath,
+  dst *path.FilePath
+) error {
 	cmd := exec.Command("cp", src.Path, dst.Path)
 	return cmd.Run()
 }
 
 // Remove: ファイルを削除する
-func (fh *FileHandler) Remove(file *FilePath) error {
+func (fh *FileHandler) Remove(
+  file *path.FilePath
+) error {
 	cmd := exec.Command("rm", file.Path)
 	return cmd.Run()
 }
