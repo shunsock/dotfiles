6e69fc2b73602cfd1ce27d88de750ef4cb9af8a7
shunsock <s.tsuchiya.business@gmail.com>
2024-10-23 11:40:53 +0900

    update alias for zsh

--- configs/zsh/config/basic/alias.zsh
+++ configs/zsh/config/basic/alias.zsh
@@ -1,32 +1,55 @@
-# Alias 'his' to show the command history with timestamps
+# -------------------------------------------------------------------
+# Basic Aliases
+# -------------------------------------------------------------------
+
+# show the command history with timestamps
 alias his='history -i'
 
-# Alias 'cd2' to go up two directories
-alias cd2='cd ../..'
-# Alias 'cd3' to go up three directories
-alias cd3='cd ../../..'
+# Short hand for 'exit' like Vim ❤️
+alias :q='exit'
 
-# Alias 'so' to source a file, which executes commands from that file in the current shell
+# Short hand for 'source'
 alias so='source'
 
-# Alias 'l' to list files in a directory, showing file types and sizes in human-readable format
+# -------------------------------------------------------------------
+# cd Command
+# -------------------------------------------------------------------
+
+alias cd2='cd ../..'
+alias cd3='cd ../../..'
+
+# -------------------------------------------------------------------
+# ls Command
+# -------------------------------------------------------------------
+
+# Shortcut for ls
 alias l='ls -Fh'
-# Alias 'la' to list all files (including hidden ones), showing file types and sizes in human-readable format
+# Showing hidden files
 alias la='ls -Fha'
-# Alias 'll' to list all files with detailed information (including file permissions, number of links, owner, group, size, and timestamp), showing sizes in human-readable format
-alias ll='ls -Fhaln'
-# Alias 'lls' to list all files sorted by size, showing detailed information and sizes in human-readable format
-alias lls='ls -FhalnS'
-# Alias 'lk' to list files showing sizes in kilobytes
-alias lk='ls -Fhk'
 
-# Alias 'v' to open files with the Vim text editor
+# -------------------------------------------------------------------
+# Vim and Neovim
+# -------------------------------------------------------------------
+
+# Shortcut for nvim
 alias v='nvim'
-# Alias 'vi' to open files with the Vim text editor
-alias vi='nvim'
+alias vi='vim'
 
-# Alias 'df' to display disk space usage for all file systems, showing sizes in kilobytes
+# -------------------------------------------------------------------
+# df Command
+#
+# change default to display disk space usage
+# for all file systems and show sizes in kilobytes
+# -------------------------------------------------------------------
+
 alias df='df -kh'
-# Alias 'du' to estimate file and directory space usage, showing sizes in kilobytes
+
+# -------------------------------------------------------------------
+# du Command
+#
+# change default to estimate file and directory space usage
+# and show sizes in kilobytes
+# -------------------------------------------------------------------
+
 alias du='du -kh'
 
