750688585cbfb7faab9280baa755d21597374095
shunsock <s.tsuchiya.business@gmail.com>
2025-04-02 17:28:07 +0900

    fix: remove colon

--- configs/zsh/config/basic/alias.zsh
+++ configs/zsh/config/basic/alias.zsh
@@ -14,7 +14,7 @@ alias sudo='nocorrect sudo'
 alias su='nocorrect su'
 
 # date output with iso8601 format
-alias date='date +"%Y-%m-%dT%H:%M:%S%:%z"'
+alias date='date +"%Y-%m-%dT%H:%M:%S%z"'
 
 # -------------------------------------------------------------------
 # cd Command
