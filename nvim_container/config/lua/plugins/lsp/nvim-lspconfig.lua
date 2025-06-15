aa721dd105561acbdeb14deda7a96629ed6befc3
shunsock <s.tsuchiya.business@gmail.com>
2025-06-15 21:13:16 +0900

    lspを追加

--- nvim_container/config/lua/plugins/lsp/nvim-lspconfig.lua
+++ nvim_container/config/lua/plugins/lsp/nvim-lspconfig.lua
@@ -9,7 +9,7 @@ return {
       { 'hrsh7th/vim-vsnip' },
     },
     config = function()
-      -- 最初にmasonをセットアップ
+      -- masonをセットアップ
       require("mason").setup({
         ui = {
           icons = {
@@ -22,7 +22,7 @@ return {
       
       -- mason-lspconfigの設定
       require("mason-lspconfig").setup({
-        ensure_installed = { "lua_ls", "pyright", "rust_analyzer" }
+        ensure_installed = { "lua_ls", "rust_analyzer" }
       })
       
       -- lspconfigの設定
@@ -32,11 +32,6 @@ return {
       -- 各サーバーの手動設定
       -- lua_ls (Lua言語サーバー)
       lspconfig.lua_ls.setup({
-        capabilities = capabilities,
-      })
-      
-      -- pyright (Python言語サーバー)
-      lspconfig.pyright.setup({
         capabilities = capabilities,
       })
       
@@ -50,18 +45,18 @@ return {
         virtual_text = false,
       })
 
-      -- Reference highlighting
-      vim.cmd [[
-        set updatetime=500
-        highlight LspReferenceText  cterm=underline ctermfg=1 ctermbg=8 gui=underline guifg=#A00000 guibg=#104040
-        highlight LspReferenceRead  cterm=underline ctermfg=1 ctermbg=8 gui=underline guifg=#A00000 guibg=#104040
-        highlight LspReferenceWrite cterm=underline ctermfg=1 ctermbg=8 gui=underline guifg=#A00000 guibg=#104040
-        augroup lsp_document_highlight
-          autocmd!
-          autocmd CursorHold,CursorHoldI * lua vim.lsp.buf.document_highlight()
-          autocmd CursorMoved,CursorMovedI * lua vim.lsp.buf.clear_references()
-        augroup END
-      ]]
+    --   -- Reference highlighting
+    --   vim.cmd [[
+    --     set updatetime=500
+    --     highlight LspReferenceText  cterm=underline ctermfg=1 ctermbg=8 gui=underline guifg=#A00000 guibg=#104040
+    --     highlight LspReferenceRead  cterm=underline ctermfg=1 ctermbg=8 gui=underline guifg=#A00000 guibg=#104040
+    --     highlight LspReferenceWrite cterm=underline ctermfg=1 ctermbg=8 gui=underline guifg=#A00000 guibg=#104040
+    --     augroup lsp_document_highlight
+    --       autocmd!
+    --       autocmd CursorHold,CursorHoldI * lua vim.lsp.buf.document_highlight()
+    --       autocmd CursorMoved,CursorMovedI * lua vim.lsp.buf.clear_references()
+    --     augroup END
+    --   ]]
     end,
   },
 }
