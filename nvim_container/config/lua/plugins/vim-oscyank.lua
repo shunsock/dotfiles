4b59d9840a5e2733c6e2b169d0edc18a01e46133
shunsock <s.tsuchiya.business@gmail.com>
2025-06-16 19:14:46 +0900

    feature: yankできない問題を解決

--- nvim_container/config/lua/plugins/vim-oscyank.lua
+++ nvim_container/config/lua/plugins/vim-oscyank.lua
@@ -11,36 +11,39 @@ return {
 
     -- プラグインをロードする前にクリップボード設定
     -- これにより最初のyank時のエラーを防止
-    vim.g.clipboard = {
-      name = 'OSC 52',
-      copy = {
-        ['+'] = function(lines)
-          require('osc52').copy(table.concat(lines, '\n'))
-        end,
-        ['*'] = function(lines)
-          require('osc52').copy(table.concat(lines, '\n'))
-        end,
-      },
-      paste = {
-        ['+'] = function()
-          return { vim.fn.getreg('+') }
-        end,
-        ['*'] = function()
-          return { vim.fn.getreg('*') }
-        end,
-      },
-      cache_enabled = true,
-    }
+    -- vim.g.clipboard = {
+    --   name = 'OSC 52',
+    --   copy = {
+    --     ['+'] = function(lines)
+    --       require('osc52').copy(table.concat(lines, '\n'))
+    --     end,
+    --     ['*'] = function(lines)
+    --       require('osc52').copy(table.concat(lines, '\n'))
+    --     end,
+    --   },
+    --   paste = {
+    --     ['+'] = function()
+    --       return { vim.fn.getreg('+') }
+    --     end,
+    --     ['*'] = function()
+    --       return { vim.fn.getreg('*') }
+    --     end,
+    --   },
+    --   cache_enabled = true,
+    -- }
     
     -- 標準のyankとpasteを確実に動作させる
     vim.opt.clipboard = "unnamedplus"
 
+    -- OSC52経由でクリップボードにコピーする機能
     local function copy()
-      if vim.v.event.operator == "y" and vim.v.event.regname == "" then
-        require("osc52").copy_register("")
+      if vim.v.event.operator == "y" then
+        local content = vim.fn.getreg(vim.v.event.regname)
+        require("osc52").copy(content)
       end
     end
 
+    -- テキストがyankされた後に実行
     vim.api.nvim_create_autocmd("TextYankPost", { callback = copy })
   end,
 }
