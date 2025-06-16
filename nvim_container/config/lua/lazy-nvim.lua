84aa976265bf91492b2c28c42d0ba9027bef6d60
shunsock <s.tsuchiya.business@gmail.com>
2025-06-16 19:01:23 +0900

    feature: autosava.nvimを追加

--- nvim_container/config/lua/lazy-nvim.lua
+++ nvim_container/config/lua/lazy-nvim.lua
@@ -13,6 +13,7 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
+  require("plugins.autosave-nvim"),
   require("plugins.catppuccin"),
   require("plugins.bufferline.setup"),
   require("plugins.copilot.setup"),
