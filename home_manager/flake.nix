1e7c77bc56894ae4507e70dc2421ef933ac08473
shunsock <s.tsuchiya.business@gmail.com>
2025-06-30 14:00:02 +0900

    home managerを追加

---
+++ home_manager/flake.nix
@@ -0,0 +1,38 @@
+{
+  description = "Home Manager configuration";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = { nixpkgs, home-manager, ... }: {
+    homeConfigurations."shunsock" = home-manager.lib.homeManagerConfiguration {
+      pkgs = import nixpkgs {
+        system = "aarch64-darwin";
+      };
+
+      username = "shunsock";
+      homeDirectory = "/Users/shunsock";
+
+      configuration = {
+        home.stateVersion = "23.11";
+        programs.home-manager.enable = true;
+
+        home.packages = with pkgs; [
+          claude-code
+          dotnetCorePackages.dotnet_9.sdk
+          git
+          go-task
+          hyperfine
+          rustup
+          tree
+        ];
+
+        programs.zsh.enable = true;
+      };
+    };
+  };
+}
+
