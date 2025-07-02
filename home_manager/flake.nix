ee31ec80bf8d26ffbdaa06dcd5b0eb618c3a8bef
shunsock <s.tsuchiya.business@gmail.com>
2025-07-03 08:50:41 +0900

    deature: zshの設定を追加

--- home_manager/flake.nix
+++ home_manager/flake.nix
@@ -7,64 +7,61 @@
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { nixpkgs, home-manager, ... }: let
-    system = "aarch64-darwin";
-    pkgs   = import nixpkgs {
-      inherit system;
-      config = { allowUnfree = true; };
-    };
-  in {
-    homeConfigurations."shunsock" = home-manager.lib.homeManagerConfiguration {
-      inherit pkgs;
-      home-manager.backupFileExtension = "backup";
+  outputs = { nixpkgs, home-manager, ... }:
+    let
+      system = "aarch64-darwin";
+      pkgs   = import nixpkgs {
+        inherit system;
+        config = { allowUnfree = true; };
+      };
+    in {
+      homeConfigurations."shunsock" = home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
+        username           = "shunsock";
+        homeDirectory      = "/Users/shunsock";
+        stateVersion       = "23.11";
+        backupFileExtension = "backup";
 
-      modules = [
-        ({ pkgs, ... }: {
-          home.username      = "shunsock";
-          home.homeDirectory = "/Users/shunsock";
-          home.stateVersion  = "23.11";
+        modules = [
+          ({ pkgs, ... }: {
+            home.packages = with pkgs; [
+              claude-code
+              dotnetCorePackages.dotnet_9.sdk
+              git
+              go-task
+              hyperfine
+              rustup
+              tree
+              zsh-autosuggestions
+              zsh-syntax-highlighting
+            ];
 
-          programs.home-manager.enable = true;
+            home.file.".config/zsh".source    = ./zsh;
+            home.file.".config/zsh".recursive = true;
 
-          home.packages = with pkgs; [
-            claude-code
-            dotnetCorePackages.dotnet_9.sdk
-            git
-            go-task
-            hyperfine
-            rustup
-            tree
-            zsh-autosuggestions
-            zsh-syntax-highlighting
-          ];
+            programs.zsh = {
+              enable = true;
 
-          # zsh 設定ファイルを再帰的にコピー
-          home.file.".config/zsh".source    = ./zsh;
-          home.file.".config/zsh".recursive = true;
+              # Oh My Zsh の有効化およびテーマ設定
+              oh-my-zsh.enable  = true;
+              oh-my-zsh.theme   = "kennethreitz";
+              oh-my-zsh.plugins = [];
 
-          programs.zsh = {
-            enable = true;
+              # Zsh 設定を再帰的に読み込む
+              initContent = ''
+                setopt globstar
+                for file in $HOME/.config/zsh/**/*.zsh; do
+                  source "$file"
+                done
+                unsetopt globstar
 
-            oh-my-zsh = {
-              enable  = true;
-              theme   = "kennethreitz";
-              plugins = [ ];
+                # zsh-autosuggestions は最後に source
+                source ${pkgs.zsh-autosuggestions}/share/zsh-autosuggestions/zsh-autosuggestions.zsh
+              '';
             };
-
-            # initExtra は非推奨なので initContent に変更
-            initContent = ''
-              setopt globstar
-              for file in $HOME/.config/zsh/**/*.zsh; do
-                source "$file"
-              done
-              unsetopt globstar
-
-              source ${pkgs.zsh-autosuggestions}/share/zsh-autosuggestions/zsh-autosuggestions.zsh
-            '';
-          };
-        })
-      ];
+          })
+        ];
+      };
     };
-  };
 }
 
