c99816f100248b1da6f1b6e7d37b49f9fae76eaa
shunsock <s.tsuchiya.business@gmail.com>
2025-07-06 14:00:41 +0900

    fix: backup

--- asagi/modules/skk.nix
+++ asagi/modules/skk.nix
@@ -1,13 +1,9 @@
 { config, pkgs, ... }:
 
 {
-  # SKK configuration files
-  home.file = {
-    "Library/Application Support/AquaSKK/BlacklistApps.plist".source = ../configs/skk/BlacklistApps.plist;
-    "Library/Application Support/AquaSKK/DictionarySet.plist".source = ../configs/skk/DictionarySet.plist;
-    "Library/Application Support/AquaSKK/SKK-JISYO.L".source = ../configs/skk/SKK-JISYO.L;
-    "Library/Application Support/AquaSKK/keymap.conf".source = ../configs/skk/keymap.conf;
-    "Library/Application Support/AquaSKK/skk-jisho.user.utf8".source = ../configs/skk/skk-jisho.user.utf8;
-    "Library/Application Support/AquaSKK/skk-jisyo.utf8".source = ../configs/skk/skk-jisyo.utf8;
+  home.file."Library/Application Support/AquaSKK" = {
+    source    = ../configs/skk;
+    recursive = true;
+    backup    = false;
   };
 }
