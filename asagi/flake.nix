c2592e7b358ef74ea334f5f84d5c535466c4d8d0
shunsock <s.tsuchiya.business@gmail.com>
2025-10-11 15:53:23 +0900

    dependency: unstable -> stable

--- asagi/flake.nix
+++ asagi/flake.nix
@@ -1,7 +1,7 @@
 {
   description = "Flake for macOS"; inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
-    nix-darwin.url = "github:LnL7/nix-darwin";
+    nix-darwin.url = "github:LnL7/nix-darwin/nix-darwin-25.05";
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
