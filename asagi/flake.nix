dd149ef30d9a8da95bf63d8776822b18614c8133
shunsock <s.tsuchiya.business@gmail.com>
2025-10-11 15:55:53 +0900

    dependency: home manager: 23 -> 25

--- asagi/flake.nix
+++ asagi/flake.nix
@@ -3,7 +3,7 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
     nix-darwin.url = "github:LnL7/nix-darwin/nix-darwin-25.05";
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
-    home-manager.url = "github:nix-community/home-manager";
+    home-manager.url = "github:nix-community/home-manager/release-25.05";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
