6da99bfb4bfcee492c9649fc691a129fbf5fd922
shunsock <s.tsuchiya.business@gmail.com>
2025-10-11 16:26:36 +0900

    feature: firefox settings

--- asagi/modules/firefox.nix
+++ asagi/modules/firefox.nix
@@ -7,31 +7,28 @@
 
     preferences = {
       "intl.accept_languages" = "en-US, en";
-      "browser.theme.selected" = "firefox-compact-dark@mozilla.org-2";
-      "browser.in-content.dark-mode" = true; 
+      "browser.theme.selected"     = "firefox-compact-dark@mozilla.org-2";
+      "browser.in-content.dark-mode" = true;
     };
 
     policies = {
-      # 検索エンジンをDuckDuckGoに設定
-      DefaultSearchEngine = "DuckDuckGo";
-      
-      # 拡張機能 Bitwarden を強制インストール
+      SearchEngines = {
+        Default = "DuckDuckGo";
+      };
+
       ExtensionSettings = {
         "bitwarden@bitwarden.com" = {
-          install_url      = "https://addons.mozilla.org/firefox/downloads/latest/bitwarden-password-manager/latest.xpi";
+          install_url       = "https://addons.mozilla.org/firefox/downloads/latest/bitwarden-password-manager/latest.xpi";
           installation_mode = "force_installed";
         };
       };
 
-      # 垂直タブの設定 (設定をロック)
       Preferences = {
-        # 'lock-true' を正しい記法に修正
-        "sidebar.revamp"      = { Value = true; Status = "locked"; };
-        "sidebar.verticalTabs" = { Value = true; Status = "locked"; };
-        
-        # サイドバーの位置をロック (false = 右側)
+        "sidebar.revamp"         = { Value = true; Status = "locked"; };
+        "sidebar.verticalTabs"   = { Value = true; Status = "locked"; };
         "sidebar.position_start" = { Value = false; Status = "locked"; };
       };
     };
   };
 }
+
