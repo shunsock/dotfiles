05aae271ad2755363fae92c2286a9d0723f39bc3
shunsock <s.tsuchiya.business@gmail.com>
2025-10-11 22:47:34 +0900

    feature: Masonのバージョンアップ対応

--- akatsuki/akatsuki-default/config/lua/plugins/lsp/nvim-lspconfig.lua
+++ akatsuki/akatsuki-default/config/lua/plugins/lsp/nvim-lspconfig.lua
@@ -1,77 +1,61 @@
 return {
   {
-    'neovim/nvim-lspconfig',
+    "neovim/nvim-lspconfig",
     dependencies = {
-      { 'williamboman/mason.nvim' },
-      { 'williamboman/mason-lspconfig.nvim' },
-      { 'hrsh7th/nvim-cmp' },
-      { 'hrsh7th/cmp-nvim-lsp' },
-      { 'hrsh7th/vim-vsnip' },
+      { "williamboman/mason.nvim" },
+      { "williamboman/mason-lspconfig.nvim" },
+      { "hrsh7th/nvim-cmp" },
+      { "hrsh7th/cmp-nvim-lsp" },
+      { "hrsh7th/vim-vsnip" },
     },
     config = function()
-      -- masonをセットアップ
+      -- mason 基本設定
       require("mason").setup({
         ui = {
           icons = {
             package_installed = "✓",
             package_pending = "➜",
-            package_uninstalled = "✗"
-          }
+            package_uninstalled = "✗",
+          },
         },
         install_root_dir = vim.fn.stdpath("data") .. "/mason",
         PATH = "append",
-        pip = {
-          install_args = {},
-        },
+        pip = { install_args = {} },
         log_level = vim.log.levels.INFO,
         max_concurrent_installers = 4,
       })
-      
-      -- mason-lspconfigの設定
+
+      -- mason-lspconfig: インストールと自動有効化
       require("mason-lspconfig").setup({
         ensure_installed = { "marksman" },
         automatic_installation = true,
+        automatic_enable = true,
       })
-      
-      -- インストール後に自動的にLSPサーバーを起動させる (APIに合わせて修正)
-      -- 古いバージョンの場合は on_server_ready を使用する
-      local has_handlers, mlsp = pcall(function()
-        local m = require("mason-lspconfig")
-        if m.setup_handlers then
-          return m
-        else
-          return nil
-        end
-      end)
-      
-      if has_handlers and mlsp then
-        mlsp.setup_handlers({
-          function(server_name)
-            require("lspconfig")[server_name].setup({})
-          end,
-        })
-      end
-      
-      -- lspconfigの設定
-      local lspconfig = require("lspconfig")
-      local capabilities = require('cmp_nvim_lsp').default_capabilities()
-      
-      -- 各サーバーの手動設定
-      -- marksman (Markdown言語サーバー)
-      lspconfig.marksman.setup({
+
+      -- nvim-cmp 連携: capabilities を付与
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+      -- サーバごとの上書き/拡張は vim.lsp.config('<name>', { ... })
+      vim.lsp.config("marksman", {
         capabilities = capabilities,
         filetypes = { "markdown", "markdown.mdx" },
-        cmd = { "marksman", "server" },
-        root_dir = function(fname)
-          return lspconfig.util.find_git_ancestor(fname) or lspconfig.util.path.dirname(fname)
-        end,
+        -- marksman は "marksman server" で起動する構成がデフォルト。
+        -- PATHに入っていれば cmd の明示は通常不要だが、必要なら以下を残す:
+        -- cmd = { "marksman", "server" },
+        -- root 検出は lspconfig の既定値で十分。独自にしたい場合だけ指定:
+        -- root_dir = require("lspconfig.util").root_pattern(".git", ".marksman.toml"),
         single_file_support = true,
       })
 
-      -- Diagnostic config
+      -- automatic_enable を使わない環境でも確実に有効化できるように明示的に enable
+      -- （既に有効なら二重起動はされず安全）
+      vim.lsp.enable({ "marksman" })
+
+      -- Diagnostic のUI調整
       vim.diagnostic.config({
         virtual_text = false,
       })
     end,
   },
 }
+
