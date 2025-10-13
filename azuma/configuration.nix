7c2f3c9588697dd270cac05ab4da627c76eeeb8b
Shunsuke Tsuchiya <s.tsuchiya.business@gmail.com>
2025-10-14 01:07:03 +0900

    fix: claude code

--- azuma/configuration.nix
+++ azuma/configuration.nix
@@ -1,8 +1,10 @@
-{ config, pkgs, ... }
+{ config, pkgs, ... }:
 
-{ imports = [ ./hardware-configuration.nix ];
+{
+  imports = [ ./hardware-configuration.nix ];
+
   nixpkgs.config.allowUnfree = true;
-
+  
   nix = {
     settings = {
       experimental-features = ["nix-command" "flakes"];
@@ -103,7 +105,7 @@
     zsh = { enable = true; };
     neovim = {
       enable = true;
-      defaultEditor = true; # $EDITOR=nvimに設定
+      defaultEditor = true;
       viAlias = true;
       vimAlias = true;
     };
@@ -112,22 +114,5 @@
     };
   };
 
-  # Some programs need SUID wrappers, can be configured further or are started in user sessions. programs.mtr.enable = true; 
-  # programs.gnupg.agent = {
-  #   enable = true; enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon. services.openssh.enable = true;
-
-  # Open ports in the firewall. networking.firewall.allowedTCPPorts = [ ... ]; networking.firewall.allowedUDPPorts = [ ... ]; Or 
-  # disable the firewall altogether. networking.firewall.enable = false;
-
-  # This value determines the NixOS release from which the default settings for stateful data, like file locations and database 
-  # versions on your system were taken. It‘s perfectly fine and recommended to leave this value at the release version of the first 
-  # install of this system. Before changing this value read the documentation for this option (e.g. man configuration.nix or on 
-  # https://nixos.org/nixos/options.html).
-  system.stateVersion = "25.05"; # Did you read the comment?
-
+  system.stateVersion = "25.05";
 }
