964d72f8b70f89774fe4afaada0f88ce6bc1dcf9
Shunsuke Tsuchiya <s.tsuchiya.business@gmail.com>
2025-10-18 15:18:50 +0900

    fix: aliases

--- azuma/configs/zsh/basic/alias.zsh
+++ azuma/configs/zsh/basic/alias.zsh
@@ -17,17 +17,6 @@ alias su='nocorrect su'
 alias date='date +"%Y-%m-%dT%H:%M:%S%z"'
 
 # -------------------------------------------------------------------
-# cd Command
-# -------------------------------------------------------------------
-
-# Note: alias . is removed as it conflicts with source/path operations
-# Use these instead for quick navigation
-alias .='cd ../'
-alias ..='cd ../../'
-alias ...='cd ../../../'
-alias ....='cd ../../../../'
-
-# -------------------------------------------------------------------
 # ls Command
 # NOTE:
 # -F 
