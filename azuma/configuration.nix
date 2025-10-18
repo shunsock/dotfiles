bf0689bb6972417ff814372914a559fdc48337d7
Shunsuke Tsuchiya <s.tsuchiya.business@gmail.com>
2025-10-18 17:13:09 +0900

    fix: remove redundnt lines

--- azuma/configuration.nix
+++ azuma/configuration.nix
@@ -19,10 +19,6 @@
   boot.loader.systemd-boot.enable = true; boot.loader.efi.canTouchEfiVariables = true;
 
   networking.hostName = "nixos"; # Define your hostname.
-  # networking.wireless.enable = true; # Enables wireless support via wpa_supplicant.
-
-  # Configure network proxy if necessary networking.proxy.default = "http://user:password@proxy:port/"; networking.proxy.noProxy = 
-  # "127.0.0.1,localhost,internal.domain";
 
   # Enable networking
   networking.networkmanager.enable = true;
@@ -42,6 +38,7 @@
    enabled = "fcitx5";
    fcitx5.addons = [pkgs.fcitx5-mozc];
   };
+
   fonts = {
     fonts = with pkgs; [
       noto-fonts-cjk-serif
@@ -76,14 +73,6 @@
   # Enable sound with pipewire.
   services.pulseaudio.enable = false; security.rtkit.enable = true; services.pipewire = {
     enable = true; alsa.enable = true; alsa.support32Bit = true; pulse.enable = true;
-    # If you want to use JACK applications, uncomment this jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default, no need to redefine it in your config 
-    # for now)
-    #media-session.enable = true;
-  };
-
-  # Enable touchpad support (enabled default in most desktopManager). services.xserver.libinput.enable = true;
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.shunsock = {
