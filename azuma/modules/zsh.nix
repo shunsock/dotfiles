d2962caa9c31d9a685633ea83b2511518b94ad38
Shunsuke Tsuchiya <s.tsuchiya.business@gmail.com>
2025-10-26 11:36:39 +0900

    feature: remove oh-my-zsh

--- azuma/modules/zsh.nix
+++ azuma/modules/zsh.nix
@@ -29,10 +29,10 @@
       unalias -m '*'
 
       # Source all custom zsh files from /etc/zsh/custom
-      setopt extendedglob
-      for f in /etc/zsh/custom/**/*.zsh; do
-        source "$f"
-      done
+      # setopt extendedglob
+      # for f in /etc/zsh/custom/**/*.zsh; do
+      #  source "$f"
+      # done
 
       # Load zsh-autosuggestions (must be loaded after other configs)
       source ${pkgs.zsh-autosuggestions}/share/zsh-autosuggestions/zsh-autosuggestions.zsh
