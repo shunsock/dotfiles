41b11f2e4a13bb01639cccd381e72e9f4ea6773e
Shunsuke Tsuchiya <s.tsuchiya.business@gmail.com>
2026-01-07 02:09:15 +0900

    feature: 一旦commit

--- asagi/configs/sketchybar/plugins/network.sh
+++ asagi/configs/sketchybar/plugins/network.sh
@@ -3,15 +3,25 @@
 # Network Plugin
 # Shows Wi-Fi connection status
 
-# Get Wi-Fi SSID
-SSID=$(networksetup -getairportnetwork en0 | sed 's/Current Wi-Fi Network: //')
+# Detect Wi-Fi interface automatically
+WIFI_DEVICE=$(networksetup -listallhardwareports | awk '/Wi-Fi/{getline; print $2}')
 
-if [[ $SSID == "You are not associated with an AirPort network."* ]]; then
+# Check if Wi-Fi interface is active
+if ifconfig $WIFI_DEVICE 2>/dev/null | grep -q "status: active"; then
+  # Get SSID from system_profiler
+  SSID=$(system_profiler SPAirPortDataType 2>/dev/null | grep -A 1 "Current Network Information:" | grep -v "Current Network Information:" | grep ":" | head -1 | awk '{print $1}' | sed 's/:$//')
+
+  if [ -z "$SSID" ]; then
+    # If SSID can't be retrieved, just show as connected
+    ICON="󰖩"
+    LABEL="Connected"
+  else
+    ICON="󰖩"
+    LABEL="$SSID"
+  fi
+else
   ICON="󰖪"
   LABEL="Disconnected"
-else
-  ICON="󰖩"
-  LABEL="$SSID"
 fi
 
 /opt/homebrew/opt/sketchybar/bin/sketchybar --set $NAME icon="$ICON" label="$LABEL"
--- asagi/modules/sketchybar.nix
+++ asagi/modules/sketchybar.nix
@@ -2,8 +2,61 @@
 
 {
   # Deploy sketchybar configuration files
-  home.file.".config/sketchybar" = {
-    source = ../configs/sketchybar;
-    recursive = true;
+  home.file.".config/sketchybar/sketchybarrc" = {
+    source = ../configs/sketchybar/sketchybarrc;
+    executable = true;
   };
+
+  home.file.".config/sketchybar/colors.sh" = {
+    source = ../configs/sketchybar/colors.sh;
+    executable = true;
+  };
+
+  # Deploy plugins individually with executable permission
+  home.file.".config/sketchybar/plugins/apple.sh" = {
+    source = ../configs/sketchybar/plugins/apple.sh;
+    executable = true;
+  };
+
+  home.file.".config/sketchybar/plugins/battery.sh" = {
+    source = ../configs/sketchybar/plugins/battery.sh;
+    executable = true;
+  };
+
+  home.file.".config/sketchybar/plugins/clock.sh" = {
+    source = ../configs/sketchybar/plugins/clock.sh;
+    executable = true;
+  };
+
+  home.file.".config/sketchybar/plugins/cpu.sh" = {
+    source = ../configs/sketchybar/plugins/cpu.sh;
+    executable = true;
+  };
+
+  home.file.".config/sketchybar/plugins/front_app.sh" = {
+    source = ../configs/sketchybar/plugins/front_app.sh;
+    executable = true;
+  };
+
+  home.file.".config/sketchybar/plugins/network.sh" = {
+    source = ../configs/sketchybar/plugins/network.sh;
+    executable = true;
+  };
+
+  home.file.".config/sketchybar/plugins/spaces.sh" = {
+    source = ../configs/sketchybar/plugins/spaces.sh;
+    executable = true;
+  };
+
+  home.file.".config/sketchybar/plugins/volume.sh" = {
+    source = ../configs/sketchybar/plugins/volume.sh;
+    executable = true;
+  };
+
+  # Activation script to reload SketchyBar
+  home.activation.reloadSketchyBar = config.lib.dag.entryAfter ["writeBoundary"] ''
+    if pgrep -x "sketchybar" > /dev/null; then
+      $DRY_RUN_CMD /opt/homebrew/bin/brew services restart sketchybar
+    fi
+  '';
 }
