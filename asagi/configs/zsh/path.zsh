d1e1b75f587abed415f7da533f670205367311c3
Shunsuke Tsuchiya <s.tsuchiya.business@gmail.com>
2026-01-26 01:11:42 +0900

    feature: clawdbotを追加

--- asagi/configs/zsh/path.zsh
+++ asagi/configs/zsh/path.zsh
@@ -1,2 +1,9 @@
 export PATH="$HOME/.nix-profile/bin:/nix/var/nix/profiles/default/bin:$PATH"
+
+# Bun
+export PATH="$HOME/.bun/bin:$PATH"
+
+# Gemini CLI
+export PATH="/etc/profiles/per-user/shunsock/bin:$PATH"
+
 export WEZTERM_CONFIG_FILE=~/.config/wezterm/wezterm.lua
