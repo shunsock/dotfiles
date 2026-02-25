37b741793eb687a598625a36af9b8d0131a42deb
Shunsuke Tsuchiya <s.tsuchiya.business@gmail.com>
2026-02-25 17:48:51 +0900

    feature: wezterm tab

--- azuma/modules/wezterm.nix
+++ azuma/modules/wezterm.nix
@@ -28,11 +28,36 @@
     config.window_background_opacity = 0.85
 
     -- Tab bar settings
-    config.window_decorations = "RESIZE"
+    config.window_decorations = "INTEGRATED_BUTTONS|RESIZE"
     config.show_new_tab_button_in_tab_bar = false
     config.show_close_tab_button_in_tabs = false
+    config.window_frame = {
+      font = wezterm.font("HackGen35 Console NF"),
+      font_size = 16.0,
+      active_titlebar_bg = "#1a1b26",
+      inactive_titlebar_bg = "#1a1b26",
+      active_titlebar_fg = "#c0caf5",
+      inactive_titlebar_fg = "#565f89",
+      border_left_width = 0,
+      border_right_width = 0,
+      border_top_height = 0,
+      border_bottom_height = 0,
+    }
     config.colors = {
        tab_bar = {
+         background = "#1a1b26",
+         active_tab = {
+           bg_color = "#24283b",
+           fg_color = "#c0caf5",
+         },
+         inactive_tab = {
+           bg_color = "#1a1b26",
+           fg_color = "#565f89",
+         },
+         inactive_tab_hover = {
+           bg_color = "#24283b",
+           fg_color = "#c0caf5",
+         },
          inactive_tab_edge = "none",
        },
     }
